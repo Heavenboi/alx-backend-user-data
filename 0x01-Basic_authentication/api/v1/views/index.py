366e5c72a7ebac24d116a4dafa95b22085fde68f
memoe <memoemukoe@gmail.com>
2024-04-17 16:49:28 +0200

    basic auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-""" Module of Index views
+
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -24,3 +26,24 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def un_recognized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+      - 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def not_allowed() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+      - 403 error
+    """
+    abort(403)
+
